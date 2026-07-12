d9855e561e90a01ea8bc11cae955c616a5595b8d
oreo <abhinavta2468@gmail.com>
2026-07-12 20:08:57 +0530

    Commit for new file systeminfo.sh

---
+++ systeminfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "\n"
+echo "<<<<<<<<<<<<<<<<System Information>>>>>>>>>>>>>>>>>"
+echo -e "\n"
+echo -e "Username :\t $USER"
+echo -e "Hostname :\t $(hostname)"
+echo -e "IP Address :\t $(hostname -i)"
+echo -e "Current date :\t $(date) "
+echo -e "\n"
+
